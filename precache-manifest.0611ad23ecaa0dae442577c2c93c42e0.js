235ec503aa074c50d4490eeade9ca6ff3acb310e
Welcome Sithole <16454247+SitholeWB@users.noreply.github.com>
2020-02-28 00:24:16 +0200

    v1

---
+++ precache-manifest.0611ad23ecaa0dae442577c2c93c42e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a0f177929414a4957277230d07bad9",
+    "url": "/sc/index.html"
+  },
+  {
+    "revision": "f2b1e140e0a8a1de9f89",
+    "url": "/sc/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "38ca42a502b999fce0af",
+    "url": "/sc/static/js/2.825f3c84.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/sc/static/js/2.825f3c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b1e140e0a8a1de9f89",
+    "url": "/sc/static/js/main.f1f18a88.chunk.js"
+  },
+  {
+    "revision": "7ab60082edd429d3d390",
+    "url": "/sc/static/js/runtime-main.8467e577.js"
+  }
+]);
