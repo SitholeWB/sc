cfcd6afb26977065fd9fc9063fcde50718332b8c
Welcome Sithole <16454247+SitholeWB@users.noreply.github.com>
2020-03-01 14:48:34 +0200

    v1.0.0.2

--- precache-manifest.4babdd013a70373158bc453fb7c590f9.js
+++ precache-manifest.4babdd013a70373158bc453fb7c590f9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "de7e123f010bd09bddeb4205cdc11c4b",
+    "revision": "a9a24609daf16911d466d0b56b35defb",
     "url": "/sc/index.html"
   },
   {
-    "revision": "4faf0d06a987c18cc2b1",
+    "revision": "6ec1ee1d86c7d4d8329a",
     "url": "/sc/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "246a17ee866f509141ff",
-    "url": "/sc/static/js/2.0899d9d3.chunk.js"
+    "revision": "da7bbb783a60111519e8",
+    "url": "/sc/static/js/2.6111c05a.chunk.js"
   },
   {
     "revision": "3adc01bea76e7956dc3633ee898f6936",
-    "url": "/sc/static/js/2.0899d9d3.chunk.js.LICENSE.txt"
+    "url": "/sc/static/js/2.6111c05a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4faf0d06a987c18cc2b1",
-    "url": "/sc/static/js/main.953b7b0f.chunk.js"
+    "revision": "6ec1ee1d86c7d4d8329a",
+    "url": "/sc/static/js/main.6deb7265.chunk.js"
   },
   {
     "revision": "7ab60082edd429d3d390",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sc/precache-manifest.d48ba48854212f667844ce3abccf612c.js"
+  "/sc/precache-manifest.4babdd013a70373158bc453fb7c590f9.js"
 );
 
 self.addEventListener('message', (event) => {
